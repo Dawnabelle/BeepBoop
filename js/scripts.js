284031247f102a5ac4d248813f6393b05b1760c2
Dawnabelle <dawn.mott@gmail.com>
2018-03-30 11:52:26 -0700

    add new workng function to js to check for 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
-// business
-function beepBoop(input){
-  var inputNumber = parseInt(input);
-  var output = "";
 
-  if (input % 3 === 0){
-    output = "I'm sorry Dave. I'm afraid I can't do that.";
-    console.log(output);
-  } 
+// business
+
+
+
+// turns input into string, asks program to check for 1, but not -1, if 1 is found, statement is true
+function checkForOnes(input){
+    var result = input.toString().indexOf(1);
+    if (result === -1) {
+      return false;
+    } else {
+      return true;
+    }
+}
+
+
+
+
+//   if (input % 3 === 0){
+//     output = "I'm sorry Dave. I'm afraid I can't do that.";
+//     console.log(output);
+//   }
 //   else if (input % 3 != 0 && input === 1 || input.charLength(i) === 1)
 // } for (var i = 0, i != %)
