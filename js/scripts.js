6d5df02ae97a8cb6aee4af5597743866b9ca6824
Dawnabelle <dawn.mott@gmail.com>
2018-03-30 10:22:30 -0700

    add a working function to javascript

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// business
+function beepBoop(input){
+  var inputNumber = parseInt(input);
+  var output = "";
+
+  if (input % 3 === 0){
+    output = "I'm sorry Dave. I'm afraid I can't do that.";
+    console.log(output);
+  } 
+//   else if (input % 3 != 0 && input === 1 || input.charLength(i) === 1)
+// } for (var i = 0, i != %)
